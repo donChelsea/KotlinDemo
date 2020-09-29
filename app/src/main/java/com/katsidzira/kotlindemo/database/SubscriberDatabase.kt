07d4135c24bf213e20e0f07ec7454d467e8816d3
Chelsea Katsidzira <ckatsidzira322@gmail.com>
2020-09-29 01:33:38 -0400

    added view elements

---
+++ app/src/main/java/com/katsidzira/kotlindemo/database/SubscriberDatabase.kt
@@ -0,0 +1,32 @@
+package com.katsidzira.kotlindemo.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Subscriber::class], version = 1)
+abstract class SubscriberDatabase : RoomDatabase() {
+
+    abstract val subscriberDAO: SubscriberDAO
+
+    // standard way to create a singleton dao for db
+
+    companion object {
+        @Volatile
+        private var INSTANCE: SubscriberDatabase? = null
+            fun getInstance(context: Context): SubscriberDatabase {
+                synchronized(this) {
+                    var instance: SubscriberDatabase? = INSTANCE
+                        if (instance == null) {
+                            instance = Room.databaseBuilder(
+                                context.applicationContext,
+                                SubscriberDatabase::class.java,
+                                "subscriber_data_table"
+                            ).build()
+                        }
+                    return instance
+                }
+            }
+    }
+}
---
+++ app/src/main/java/com/katsidzira/kotlindemo/repository/SubscriberRepository.kt
@@ -0,0 +1,25 @@
+package com.katsidzira.kotlindemo.repository
+
+import com.katsidzira.kotlindemo.database.Subscriber
+import com.katsidzira.kotlindemo.database.SubscriberDAO
+
+class SubscriberRepository(private val dao: SubscriberDAO) {
+
+    val subscriber = dao.getAllSubscribers()
+
+    suspend fun insert(subscriber: Subscriber) {
+        dao.insertSubscriber(subscriber)
+    }
+
+    suspend fun update(subscriber: Subscriber) {
+        dao.updateSubscriber(subscriber)
+    }
+
+    suspend fun delete(subscriber: Subscriber) {
+        dao.deleteSubscriber(subscriber)
+    }
+
+    suspend fun deleteAll() {
+        dao.deleteAll()
+    }
+}
