3a22b733ad25a44053e3b833e379a7a1a974dd2f
Chelsea Katsidzira <ckatsidzira322@gmail.com>
2020-09-30 00:07:22 -0400

    views are changing depending on subscriber value and cleaned up xml layouts

--- app/src/main/java/com/katsidzira/kotlindemo/viewmodel/SubscriberViewModel.kt
+++ app/src/main/java/com/katsidzira/kotlindemo/viewmodel/SubscriberViewModel.kt
@@ -1,6 +1,5 @@
 package com.katsidzira.kotlindemo.viewmodel
 
-import androidx.databinding.BaseObservable
 import androidx.databinding.Bindable
 import androidx.databinding.Observable
 import androidx.lifecycle.MutableLiveData
@@ -13,13 +12,18 @@ import kotlinx.coroutines.launch
 class SubscriberViewModel(private val repository: SubscriberRepository) : ViewModel(), Observable {
 
     val subscribers = repository.subscribers
+    private var isUpdateOrDelete = false
+    private lateinit var subscriberToUpdateOrDelete: Subscriber
 
     @Bindable
     val inputName = MutableLiveData<String>()
+
     @Bindable
     val inputEmail = MutableLiveData<String>()
+
     @Bindable
     val saveOrUpdateButtonText = MutableLiveData<String>()
+
     @Bindable
     val clearAllOrDeleteButtonText = MutableLiveData<String>()
 
@@ -28,35 +32,69 @@ class SubscriberViewModel(private val repository: SubscriberRepository) : ViewMo
         clearAllOrDeleteButtonText.value = "Clear all"
     }
 
-    fun saveOrUpdate() {
-        val name = inputName.value!!
-        val email = inputEmail.value!!
-
-        insert(Subscriber(0, name, email))
-
-        inputName.value = null
-        inputEmail.value = null
-    }
-
-    fun clearAllOrDelete() {
-        clearAll()
-    }
-
-
     fun insert(subscriber: Subscriber) = viewModelScope.launch {
-            repository.insert(subscriber)
+        repository.insert(subscriber)
+        resetViews()
     }
 
     fun update(subscriber: Subscriber) = viewModelScope.launch {
-            repository.update(subscriber)
+        repository.update(subscriber)
     }
 
     fun delete(subscriber: Subscriber) = viewModelScope.launch {
-            repository.delete(subscriber)
+        repository.delete(subscriber)
+        resetViews()
     }
 
     fun clearAll() = viewModelScope.launch {
         repository.deleteAll()
+    }
+
+    fun changeViewForSubscriber(subscriber: Subscriber) {
+        inputName.value = subscriber.name
+        inputEmail.value = subscriber.email
+
+        isUpdateOrDelete = true
+
+        subscriberToUpdateOrDelete = subscriber
+
+        saveOrUpdateButtonText.value = "Update"
+        clearAllOrDeleteButtonText.value = "Delete"
+    }
+
+    fun saveOrUpdate() {
+        if (isUpdateOrDelete) {
+            subscriberToUpdateOrDelete.name = inputName.value!!
+            subscriberToUpdateOrDelete.email = inputEmail.value!!
+
+            update(subscriberToUpdateOrDelete)
+        } else {
+            val name = inputName.value!!
+            val email = inputEmail.value!!
+
+            insert(Subscriber(0, name, email))
+
+            inputName.value = null
+            inputEmail.value = null
+        }
+    }
+
+    fun clearAllOrDelete() {
+        if (isUpdateOrDelete) {
+            delete(subscriberToUpdateOrDelete)
+        } else {
+            clearAll()
+        }
+    }
+
+    private fun resetViews() {
+        inputName.value = null
+        inputEmail.value = null
+
+        isUpdateOrDelete = false
+
+        saveOrUpdateButtonText.value = "Save"
+        clearAllOrDeleteButtonText.value = "Clear all"
     }
 
     override fun removeOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) {
